d893fd6e70d7b5237b2d8e4886845db339f44c45
Brian Burridge <bburridg@gmail.com>
2010-08-31 16:45:49 -0400

    Add preferred gems

--- headstart.rb
+++ headstart.rb
@@ -1,5 +1,10 @@
 gem "cops", :version => '0.2.0.7', :lib => 'blue_light_special'
-
+gem 'formtastic'
+gem 'will_paginate'
+# must use paperclip 2.3.0 at this point due to conflict between aws-s3 (needed by paperclip 2.3.1 and right_aws needed by backup_fu plugin)
+gem 'paperclip', :version => '2.3.0'
+gem 'right_aws'
+  
 generate :blue_light_special
 
 if yes?("Do you want to use Delayed Job?")
