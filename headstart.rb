2694f4e056823e9a0fb7dcd7e6ff3f20f5b01711
Brian Burridge <bburridg@gmail.com>
2010-09-01 13:51:49 -0400

    Switched to installing headstart gem

--- headstart.rb
+++ headstart.rb
@@ -1,11 +1,11 @@
-gem "cops", :version => '0.2.0.7', :lib => 'blue_light_special'
+gem "headstart", :version => '0.1.0'
 gem 'formtastic'
 gem 'will_paginate'
 # must use paperclip 2.3.0 at this point due to conflict between aws-s3 (needed by paperclip 2.3.1 and right_aws needed by backup_fu plugin)
 gem 'paperclip', :version => '2.3.0'
 gem 'right_aws'
   
-generate :blue_light_special
+generate :headstart
 
 if yes?("Do you want to use Delayed Job?")
   generate :delayed_job
@@ -14,7 +14,7 @@ else
   delayed_job = false
 end
 
-source_file = File.expand_path(File.dirname(__FILE__)) + "/config/blue_light_special.yml"
+source_file = File.expand_path(File.dirname(__FILE__)) + "/config/headstart.yml"
 
 contents = ""
 File.open(source_file, 'r') do |f1|  
@@ -26,12 +26,12 @@ File.open(source_file, 'r') do |f1|
    end  
 end
 
-File.open('config/blue_light_special.yml', 'w') do |f2|   
+File.open('config/headstart.yml', 'w') do |f2|   
  f2.puts contents
 end
 
 
 rake("db:migrate")
 
-generate :blue_light_special_admin
+generate :headstart_admin
 
