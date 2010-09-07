0da7ab254c86a39e8172be96e598effff051fba9
Brian Burridge <bburridg@gmail.com>
2010-09-07 17:15:35 -0400

    Use .2 version of headstart

--- headstart.rb
+++ headstart.rb
@@ -1,4 +1,4 @@
-gem "headstart", :version => '0.1.0'
+gem "headstart", :version => '0.2.0'
 gem 'formtastic'
 gem 'will_paginate'
 # must use paperclip 2.3.0 at this point due to conflict between aws-s3 (needed by paperclip 2.3.1 and right_aws needed by backup_fu plugin)
