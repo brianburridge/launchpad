c67c26643a1483c52b9897d069df5fe5eb4e1dc9
Brian Burridge <bburridg@gmail.com>
2010-09-09 19:42:41 -0400

    Test for existence of settings table before initizalise global vars

--- global_settings.rb
+++ global_settings.rb
@@ -13,10 +13,12 @@ title = current_app_name if title.blank?
 description = ask("Web app description (used on home page)?")
 
 file "config/initializers/global_settings.rb", <<-END
+if  ActiveRecord::Base.connection.tables.include?('settings')
  # Place global settings here like
  Settings.page_limit = 10
  Settings.title = "#{title}"
  Settings.description = "#{description}"
+end
 END
 
 end
