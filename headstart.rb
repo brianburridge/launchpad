64ec75c28eae38c0301871593e2d68395f75b640
Brian Burridge <bburridg@gmail.com>
2010-09-22 18:51:53 -0400

    Upgraded headstart

--- headstart.rb
+++ headstart.rb
@@ -1,4 +1,4 @@
-gem "headstart", :version => '0.6.0'
+gem "headstart", :version => '0.6.2'
 gem 'formtastic', :version => '0.9.7'
 gem 'will_paginate', :version => '2.2.2'
 gem 'jammit', :version => '0.5.1'
