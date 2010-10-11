7483b6ce53f8a4f1cf57a973e55d5e29bd8fc0a9
Brian Burridge <bburridg@gmail.com>
2010-10-11 17:42:47 -0400

    Skip backups to amazon for now

--- launch_all.rb
+++ launch_all.rb
@@ -4,7 +4,7 @@ load_template "http://github.com/bburridge/launchpad/raw/master/use_facebook_aut
 load_template "http://github.com/bburridge/launchpad/raw/master/cleanup.rb"
 load_template "http://github.com/bburridge/launchpad/raw/master/file_attachments.rb"
 load_template "http://github.com/bburridge/launchpad/raw/master/global_settings.rb"
-load_template "http://github.com/bburridge/launchpad/raw/master/setup_backups_to_amazons3.rb"
+#load_template "http://github.com/bburridge/launchpad/raw/master/setup_backups_to_amazons3.rb"
 load_template "http://github.com/bburridge/launchpad/raw/master/woopra_and_google_analytics.rb"
 load_template "http://github.com/bburridge/launchpad/raw/master/add_zendesk.rb"
 load_template "http://github.com/bburridge/launchpad/raw/master/git_setup.rb"
