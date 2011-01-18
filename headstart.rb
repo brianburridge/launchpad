58f74a0ddbf8d0902f8d4d5e7878a440b7a9ecbb
Brian Burridge <bburridg@gmail.com>
2011-01-17 23:45:35 -0500

    Upgraded to using headstart 11.2

--- headstart.rb
+++ headstart.rb
@@ -1,4 +1,4 @@
-gem "headstart", :version => '0.9.2'
+gem "headstart", :version => '0.11.2'
 gem 'formtastic', :version => '0.9.7'
 gem 'will_paginate', :version => '2.2.2'
 gem 'jammit', :version => '0.5.1'
